5609cf0ec5a17697e562bf4a0f76ee04b9756f09
Stufzx <cardonasierrarafaelantonio7@gmail.com>
2025-04-04 21:36:37 -0500

    initial commit

---
+++ Launcher Cocainomano.spec
@@ -0,0 +1,40 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['main.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='Launcher Cocainomano',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    version='C:\\Users\\Rafa\\AppData\\Local\\Temp\\a7f9ce9e-92b7-4596-9d80-34de383a377f',
+    icon=['icono.ico'],
+)
---
+++ main.py
@@ -0,0 +1,246 @@
+import flet as ft
+import subprocess
+import json
+import os
+
+DATA_FILE = "juegos.json"
+
+def main(page: ft.Page):
+    
+    page.title = "Launcher de juegos"
+    page.window.title_bar_hidden = True
+    page.window.title_bar_buttons_hidden = True
+    page.window.center()
+    page.padding = 20
+    page.bgcolor = ft.colors.WHITE
+    page.window.width = 1280
+    page.window.height = 900
+
+    # Fuente personalizada (asegúrate de que la ruta sea correcta)
+    page.fonts = {
+        "MiFuente": "fuentes\\WinkySans-VariableFont_wght.ttf"
+    }
+    page.theme = ft.Theme(font_family="MiFuente")
+
+    # Función para cerrar la ventana
+    def cerrar_app(e):
+        page.window.close()
+
+    # Botón de cierre
+    boton_cerrar = ft.IconButton(
+        icon=ft.icons.CLOSE,
+        tooltip="Cerrar",
+        on_click=cerrar_app,
+        icon_color=ft.colors.RED,
+        icon_size=24,
+        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10)),
+    )
+
+    # Añadir el botón arriba a la derecha
+    page.add(
+        ft.Row(
+            controls=[boton_cerrar],
+            alignment=ft.MainAxisAlignment.END
+        )
+    )
+
+
+    scroll_state = {"x": 0}
+    juegos = []
+
+    def handle_close(e):
+        page.close(dlg_modal)
+
+    dlg_modal = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Ayuda"),
+        content=ft.Text("Imagen Recomendada 1024x1024"),
+        actions=[
+            ft.TextButton("Cerrar", on_click=handle_close),
+           # ft.TextButton("No", on_click=handle_close),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+        #on_dismiss=lambda e: page.add(
+           # ft.Text("Modal dialog dismissed"),
+        #),
+    )
+
+    def mostrar_ayuda(e):
+        page.open(dlg_modal)
+        #page.update()
+
+    def guardar_juegos():
+        with open(DATA_FILE, "w", encoding="utf-8") as f:
+            json.dump(juegos, f, indent=4)
+
+    def crear_container_juego(imagen_path="", juego_path=None, titulo=""):
+        imagen_juego = ft.Image(
+            src=imagen_path,
+            width=300,
+            height=300,
+            fit=ft.ImageFit.CONTAIN,
+            border_radius=ft.border_radius.all(30),
+            #alignment=ft.alignment.center,
+            
+        )
+        
+        ruta_juego = {"ruta": juego_path}
+        titulo_control = ft.TextField(
+            value=titulo,
+            text_align=ft.TextAlign.CENTER,
+            on_change=lambda e: actualizar_info_juego(index),
+            width=480,
+            border="none",
+            hint_text="Titulo del Juego",   
+            #border_radius=ft.border_radius.all(10),
+            #filled=True,
+            #label="Título del juego",
+        )
+
+        def actualizar_info_juego(i):
+            juegos[i] = {
+                "titulo": titulo_control.value,
+                "imagen": imagen_juego.src,
+                "juego": ruta_juego["ruta"]
+            }
+            guardar_juegos()
+
+        def subir_imagen(e):
+            def on_result(e: ft.FilePickerResultEvent):
+                if e.files:
+                    imagen_juego.src = e.files[0].path
+                    actualizar_info_juego(index)
+                    imagen_juego.update()
+
+            picker = ft.FilePicker(on_result=on_result)
+            page.overlay.append(picker)
+            page.update()
+            picker.pick_files(allow_multiple=False)
+
+        def cargar_ruta_juego(e):
+            def on_result(e: ft.FilePickerResultEvent):
+                if e.files:
+                    ruta_juego["ruta"] = e.files[0].path
+                    actualizar_info_juego(index)
+
+            picker = ft.FilePicker(on_result=on_result)
+            page.overlay.append(picker)
+            page.update()
+            picker.pick_files(allowed_extensions=["exe"])
+        def eliminar_juego(index, contenedor):
+            if 0 <= index < len(juegos):
+                del juegos[index]
+                row.controls.remove(contenedor)
+                guardar_juegos()
+                row.update()
+        def lanzar_juego(e):
+            if ruta_juego["ruta"]:
+                try:
+                    subprocess.Popen(ruta_juego["ruta"])
+                except Exception as err:
+                    print("Error al ejecutar:", err)
+
+        index = len(juegos)
+        juegos.append({
+            "titulo": titulo,
+            "imagen": imagen_path,
+            "juego": juego_path
+        })
+
+        contenedor = ft.Container(
+            content=ft.Column([
+                titulo_control,
+                imagen_juego,
+                ft.Row([
+                    ft.ElevatedButton("Subir imagen", on_click=subir_imagen,style=ft.ButtonStyle(
+            color=ft.colors.WHITE,
+            bgcolor=ft.colors.DEEP_PURPLE,
+            padding=20,
+            elevation=5,
+            overlay_color=ft.colors.PURPLE_100,
+            shape=ft.RoundedRectangleBorder(radius=20),
+        ),),
+                    ft.ElevatedButton("Cargar juego", on_click=cargar_ruta_juego),
+                    ft.ElevatedButton("Eliminar", icon=ft.icons.DELETE, style=ft.ButtonStyle(bgcolor=ft.colors.RED_100),
+                                    on_click=lambda e: eliminar_juego(index, contenedor)),
+                ])
+
+            ],
+                alignment=ft.MainAxisAlignment.CENTER),
+            width=350,
+            height=500,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.TRANSPARENT,
+            border_radius=10,
+            margin=5,
+            on_click=lanzar_juego,
+            animate_scale=ft.Animation(300, "easeInOut")
+        )
+
+        def on_hover(e: ft.HoverEvent):
+            contenedor.scale = 1.05 if e.data == "true" else 1
+            contenedor.update()
+
+        contenedor.on_hover = on_hover
+        return contenedor
+
+
+    row = ft.Row(
+        controls=[],
+        spacing=10,
+    )
+
+
+
+    def agregar_juego(e=None):
+        nuevo = crear_container_juego()
+        row.controls.append(nuevo)
+        row.update()
+        guardar_juegos()
+
+    def cargar_juegos_guardados():
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                for juego in data:
+                    cont = crear_container_juego(
+                        juego.get("imagen", ""),
+                        juego.get("juego", ""),
+                        juego.get("titulo", "")
+                    )
+                    row.controls.append(cont)
+            row.update()
+
+
+    scroll_container = ft.Container(
+        content=row,
+        width=page.width,
+        height=700,
+    )
+
+    def on_resize(e):
+        scroll_container.width = page.width
+        scroll_container.update()
+
+    def on_pan_update(e: ft.DragUpdateEvent):
+        scroll_state["x"] += e.delta_x
+        row.offset = ft.Offset(scroll_state["x"] / 1000, 0)
+        row.update()
+
+    gesture = ft.GestureDetector(
+        content=scroll_container,
+        on_pan_update=on_pan_update,
+    )
+
+    page.on_resize = on_resize
+    page.add(
+        gesture,
+        ft.Row([
+            ft.ElevatedButton("Añadir juego", icon=ft.icons.ADD, on_click=agregar_juego),
+            ft.ElevatedButton("Ayuda", icon=ft.icons.HELP, on_click=mostrar_ayuda),
+        ])
+    )
+
+    cargar_juegos_guardados()
+
+ft.app(target=main)
